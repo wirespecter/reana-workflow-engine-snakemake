0ea0ccc1eee70a892629e7fbd161bce0189b2fe7
Audrius Mecionis <audrius.mecionis@gmail.com>
2023-03-09 10:37:28 +0100

    installation: bump shared modules

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ setup_requires = [
 ]
 
 install_requires = [
-    "reana-commons[snakemake_reports]>=0.9.1,<0.10.0",
+    "reana-commons[snakemake_reports]>=0.9.3a1,<0.10.0",
 ]
 
 packages = find_packages()
