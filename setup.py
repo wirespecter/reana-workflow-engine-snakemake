9f37894a1faf9ef60cb62d8a50471ad89a8fb6b9
Marco Donadoni <marco.donadoni@cern.ch>
2024-07-30 09:38:51 +0200

    build(python): use optional deps instead of `tests_require` (#100)
    Closes reanahub/reana#814

--- setup.py
+++ setup.py
@@ -18,10 +18,6 @@ from setuptools import find_packages, setup
 readme = open("README.md").read()
 history = open("CHANGELOG.md").read()
 
-tests_require = [
-    "pytest-reana>=0.95.0a2,<0.96.0",
-]
-
 extras_require = {
     "debug": [
         "wdb",
@@ -33,7 +29,9 @@ extras_require = {
         "Sphinx>=1.5.1",
         "sphinx-rtd-theme>=0.1.9",
     ],
-    "tests": tests_require,
+    "tests": [
+        "pytest-reana>=0.95.0a2,<0.96.0",
+    ],
     "xrootd": [
         "xrootd==5.6.0",
     ],
@@ -80,7 +78,6 @@ setup(
     },
     python_requires=">=3.8",
     extras_require=extras_require,
-    tests_require=tests_require,
     classifiers=[
         "Development Status :: 3 - Alpha",
         "Environment :: Web Environment",
