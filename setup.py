14dec1b96cb9be542a4e99a84dfc85819eae5c1f
Marco Donadoni <marco.donadoni@cern.ch>
2024-07-30 09:28:24 +0200

    build(python): remove deprecated `pytest-runner` (#100)
    Closes reanahub/reana#814

--- setup.py
+++ setup.py
@@ -45,11 +45,6 @@ for key, reqs in extras_require.items():
         continue
     extras_require["all"].extend(reqs)
 
-
-setup_requires = [
-    "pytest-runner>=2.7",
-]
-
 install_requires = [
     "reana-commons[snakemake_reports]>=0.95.0a2,<0.96.0",
 ]
@@ -85,7 +80,6 @@ setup(
     },
     python_requires=">=3.8",
     extras_require=extras_require,
-    setup_requires=setup_requires,
     tests_require=tests_require,
     classifiers=[
         "Development Status :: 3 - Alpha",
